a5860d16747cca7760436368f13455d057b216b6
Marcosbertolinni <marcosbertolinni@gmail.com>
2026-05-10 03:20:45 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Meu primeiro projeto Java!");
 
          int opcao = 0;
